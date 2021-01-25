df4494b7b56528e3d56383f82d70dbb39d105922
Satanchik <satanchikk@gmail.com>
2021-01-25 19:24:28 +0200

    added top-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
-$(function () {
+$(function(){
+
+   $('.top-slider__inner').slick({
+      dots: true,
+      arrows: false,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 2000
+   });
 
 });
